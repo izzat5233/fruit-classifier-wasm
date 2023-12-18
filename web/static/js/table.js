a8116b4e2cda9cc3ad575433cb1c239c798f647f
Izzat <izzat5233@gmail.com>
2023-12-18 21:31:41 +0200

    More robust table filler functions, they combine data in one table.

--- web/static/js/table.js
+++ web/static/js/table.js
@@ -1,19 +1,23 @@
+function setupTableHeader(theadSelector, numCols, name = "") {
+    const thead = d3.select(theadSelector);
+    let tr = thead.selectAll("tr").join();
+    if (tr.empty()) tr = thead.append('tr');
+
+    for (let i = 0; i < numCols; i++) {
+        tr.append('th').text(name + (i + 1));
+    }
+}
+
 function fillNumTable(tbodySelector, vecVecNum, decimalPlaces = 4) {
     const tbody = d3.select(tbodySelector);
-    for (let i = 0; i < vecVecNum.size(); i++) {
-        const tr = tbody.append('tr');
-        for (let j = 0; j < vecVecNum.get(i).size(); j++) {
+    for (let i = 0; i < vecVecNum.size(); ++i) {
+        let tr = tbody.selectAll(`tr:nth-child(${i + 1})`);
+        if (tr.empty()) tr = tbody.append('tr');
+
+        for (let j = 0; j < vecVecNum.get(i).size(); ++j) {
             const num = vecVecNum.get(i).get(j);
             const formattedNum = typeof num === 'number' ? num.toFixed(decimalPlaces) : num;
             tr.append('td').text(formattedNum);
         }
-    }
-}
-
-function setupTableHeader(theadSelector, numCols, name = "") {
-    const thead = d3.select(theadSelector);
-    const tr = thead.append('tr');
-    for (let i = 0; i < numCols; i++) {
-        tr.append('th').text(name + (i + 1)); // Headers numbered 1, 2, 3, ...
     }
 }
